ab1bbe19b35ddcc98bb32570ca144740ba7bed9e
Giang (Zach) Nguyen <zach@dataxight.com>
2025-07-08 15:24:56 +0700

    Update log level

--- gcp_variant_transforms/libs/variant_sharding.py
+++ gcp_variant_transforms/libs/variant_sharding.py
@@ -278,7 +278,7 @@ class VariantSharding():
       shard_index = self._region_to_shard.get(chrom, _UNDEFINED_SHARD_INDEX)
 
     if shard_index == _UNDEFINED_SHARD_INDEX:
-      logging.error(f"Position {pos} on chromosome {chrom} left as residual")
+      logging.warning(f"Position {pos} on chromosome {chrom} left as residual")
       return self._residual_index
     else:
       return shard_index
